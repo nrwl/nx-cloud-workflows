3db5888faba573e17984d12482b0ebc2c9a54c1b
Rares Matei <matei.rar@gmail.com>
2023-10-11 14:39:27 +0100

    add husky and prepush check

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -1,7 +1,7 @@
 const {execSync} = require("child_process");
 
-execSync('npx nx run-many -t build')
-const output = execSync(`git status --porcelain | grep '/output'`).toString('utf-8');
+execSync('npx nx run-many -t build', {stdio: 'inherit'})
+const output = execSync(`git status --porcelain | grep '/output' || true`).toString('utf-8');
 
 if (output) {
     console.error("Source files have been modified. Commit them first.");
