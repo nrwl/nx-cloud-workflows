d56aa64d87686deed891db86437504c7f1a56b1f
James Henry <james@henry.sc>
2023-12-07 13:46:16 +0400

    feat: add support for special checkout depth 0 and fetch tags

--- workflow-steps/checkout/main.ts
+++ workflow-steps/checkout/main.ts
@@ -1,15 +1,27 @@
-import { execSync } from 'child_process';
+import { execSync } from "node:child_process";
 
 const repoUrl = process.env.GIT_REPOSITORY_URL;
 const commitSha = process.env.NX_COMMIT_SHA;
 const commitRef = process.env.NX_COMMIT_REF;
 const branch = process.env.NX_BRANCH;
 const depth = process.env.GIT_CHECKOUT_DEPTH || 1;
+const fetchTags = process.env.GIT_FETCH_TAGS === "true";
 
 execSync(`git config --global --add safe.directory /home/workflows/workspace`);
-execSync('git init .');
+execSync("git init .");
 execSync(`git remote add origin ${repoUrl}`);
-execSync(
-  `git fetch --no-tags --prune --progress --no-recurse-submodules --depth=${depth} origin +${commitSha}:${commitRef}`
-);
+
+if (depth === "0") {
+  // Fetch all history and tags if depth is 0
+  execSync(
+    "git fetch --prune --progress --no-recurse-submodules --tags origin"
+  );
+} else {
+  // Fetch with specified depth
+  const tagOption = fetchTags ? "--tags" : "--no-tags";
+  execSync(
+    `git fetch ${tagOption} --prune --progress --no-recurse-submodules --depth=${depth} origin +${commitSha}:${commitRef}`
+  );
+}
+
 execSync(`git checkout --progress --force -B ${branch} ${commitRef}`);
