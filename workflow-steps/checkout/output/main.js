ff45e441932baff23084e1346fcdf07cbbbed808
“JamesHenry” <james@henry.sc>
2023-12-07 14:42:51 +0400

    chore: updates

--- workflow-steps/checkout/output/main.js
+++ workflow-steps/checkout/output/main.js
@@ -6,12 +6,6 @@ var commitRef = process.env.NX_COMMIT_REF;
 var branch = process.env.NX_BRANCH;
 var depth = process.env.GIT_CHECKOUT_DEPTH ?? 1;
 var fetchTags = process.env.GIT_FETCH_TAGS === "true";
-console.log({
-  rawDepth: process.env.GIT_CHECKOUT_DEPTH,
-  resolvedDepth: depth,
-  to: typeof process.env.GIT_CHECKOUT_DEPTH,
-  rto: typeof depth
-});
 (0, import_child_process.execSync)(`git config --global --add safe.directory /home/workflows/workspace`);
 (0, import_child_process.execSync)("git init .");
 (0, import_child_process.execSync)(`git remote add origin ${repoUrl}`);
@@ -25,4 +19,4 @@ if (Number(depth) === 0) {
     `git fetch ${tagOption} --prune --progress --no-recurse-submodules --depth=${depth} origin +${commitSha}:${commitRef}`
   );
 }
-(0, import_child_process.execSync)(`git checkout --progress --force -B ${branch} ${commitRef}`);
+(0, import_child_process.execSync)(`git checkout --progress --force -B ${branch} ${commitSha}`);
