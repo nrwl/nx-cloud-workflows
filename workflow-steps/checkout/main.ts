846b49ef34208d729ddafd9c82d1b5d23467d523
James Henry <james@henry.sc>
2023-12-07 14:00:42 +0400

    Update main.ts

--- workflow-steps/checkout/main.ts
+++ workflow-steps/checkout/main.ts
@@ -7,6 +7,8 @@ const branch = process.env.NX_BRANCH;
 const depth = process.env.GIT_CHECKOUT_DEPTH || 1;
 const fetchTags = process.env.GIT_FETCH_TAGS === "true";
 
+console.log({ depth, to: typeof depth });
+
 execSync(`git config --global --add safe.directory /home/workflows/workspace`);
 execSync("git init .");
 execSync(`git remote add origin ${repoUrl}`);
