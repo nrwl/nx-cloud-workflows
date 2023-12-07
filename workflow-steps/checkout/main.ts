2e66093c0ec36bafc5e3438f0a4cf117a1176ec4
“JamesHenry” <james@henry.sc>
2023-12-07 15:10:05 +0400

    chore: wip

--- workflow-steps/checkout/main.ts
+++ workflow-steps/checkout/main.ts
@@ -1,7 +1,8 @@
-import { execSync } from 'node:child_process';
+import { execSync } from 'child_process';
 
 const repoUrl = process.env.GIT_REPOSITORY_URL;
-const commitSha = process.env.NX_COMMIT_SHA;
+const commitSha = process.env.NX_COMMIT_SHA; // Commit SHA for PR or branch
+const isPR = true; // New variable to determine if it's a PR
 const branch = process.env.NX_BRANCH;
 const depth = process.env.GIT_CHECKOUT_DEPTH || 1;
 
@@ -10,14 +11,24 @@ execSync('git init .');
 execSync(`git remote add origin ${repoUrl}`);
 
 if (depth === '0') {
-  // Fetch all history, all branches, and tags if depth is 0
+  // Fetch all branches and tags if depth is 0
   execSync(
     'git fetch --prune --progress --no-recurse-submodules --tags origin "+refs/heads/*:refs/remotes/origin/*"',
   );
-  // Checkout the branch
-  execSync(`git checkout --progress --force -B ${branch} origin/${branch}`);
+  if (isPR) {
+    // Fetch PR specific references for GitHub
+    execSync(
+      `git fetch origin pull/${commitSha}/head:refs/remotes/origin/pr/${commitSha}`,
+    );
+  }
+  // Checkout the commit directly if it's a PR; otherwise, checkout the branch
+  execSync(
+    `git checkout --progress --force ${
+      isPR ? 'refs/remotes/origin/pr/' + commitSha : commitSha
+    }`,
+  );
 } else {
-  // Fetch with specified depth
+  // Fetch with specified depth for branch
   execSync(
     `git fetch --no-tags --prune --progress --no-recurse-submodules --depth=${depth} origin ${branch}`,
   );
--- workflow-steps/checkout/output/main.js
+++ workflow-steps/checkout/output/main.js
@@ -1,20 +1,28 @@
 // main.ts
-var import_node_child_process = require("node:child_process");
+var import_child_process = require("child_process");
 var repoUrl = process.env.GIT_REPOSITORY_URL;
 var commitSha = process.env.NX_COMMIT_SHA;
+var isPR = true;
 var branch = process.env.NX_BRANCH;
 var depth = process.env.GIT_CHECKOUT_DEPTH || 1;
-(0, import_node_child_process.execSync)(`git config --global --add safe.directory /home/workflows/workspace`);
-(0, import_node_child_process.execSync)("git init .");
-(0, import_node_child_process.execSync)(`git remote add origin ${repoUrl}`);
+(0, import_child_process.execSync)(`git config --global --add safe.directory /home/workflows/workspace`);
+(0, import_child_process.execSync)("git init .");
+(0, import_child_process.execSync)(`git remote add origin ${repoUrl}`);
 if (depth === "0") {
-  (0, import_node_child_process.execSync)(
+  (0, import_child_process.execSync)(
     'git fetch --prune --progress --no-recurse-submodules --tags origin "+refs/heads/*:refs/remotes/origin/*"'
   );
-  (0, import_node_child_process.execSync)(`git checkout --progress --force -B ${branch} origin/${branch}`);
+  if (isPR) {
+    (0, import_child_process.execSync)(
+      `git fetch origin pull/${commitSha}/head:refs/remotes/origin/pr/${commitSha}`
+    );
+  }
+  (0, import_child_process.execSync)(
+    `git checkout --progress --force ${isPR ? "refs/remotes/origin/pr/" + commitSha : commitSha}`
+  );
 } else {
-  (0, import_node_child_process.execSync)(
+  (0, import_child_process.execSync)(
     `git fetch --no-tags --prune --progress --no-recurse-submodules --depth=${depth} origin ${branch}`
   );
-  (0, import_node_child_process.execSync)(`git checkout --progress --force -B ${branch} origin/${branch}`);
+  (0, import_child_process.execSync)(`git checkout --progress --force -B ${branch} origin/${branch}`);
 }
