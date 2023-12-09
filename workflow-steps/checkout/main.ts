2e22e57a5dfc2532f57b9bad7c68bd8fe3faa9ff
James Henry <james@henry.sc>
2023-12-09 14:40:53 +0400

    feat: add support for special checkout depth 0 and fetch tags (#22)

--- workflow-steps/checkout/main.ts
+++ workflow-steps/checkout/main.ts
@@ -2,14 +2,39 @@ import { execSync } from 'child_process';
 
 const repoUrl = process.env.GIT_REPOSITORY_URL;
 const commitSha = process.env.NX_COMMIT_SHA;
-const commitRef = process.env.NX_COMMIT_REF;
-const branch = process.env.NX_BRANCH;
+const nxBranch = process.env.NX_BRANCH; // This can be a PR number or a branch name
 const depth = process.env.GIT_CHECKOUT_DEPTH || 1;
+const fetchTags = process.env.GIT_FETCH_TAGS === 'true';
+
+// TODO: infer this in cases where the NX_BRANCH is a branch name despite being a PR (certain CI providers, not GitHub)
+const isPR = !isNaN(parseInt(nxBranch!));
 
 execSync(`git config --global --add safe.directory /home/workflows/workspace`);
 execSync('git init .');
 execSync(`git remote add origin ${repoUrl}`);
-execSync(
-  `git fetch --no-tags --prune --progress --no-recurse-submodules --depth=${depth} origin +${commitSha}:${commitRef}`
-);
-execSync(`git checkout --progress --force -B ${branch} ${commitRef}`);
+
+if (depth === '0') {
+  // Fetch all branches and tags if depth is 0
+  execSync(
+    'git fetch --prune --progress --no-recurse-submodules --tags origin "+refs/heads/*:refs/remotes/origin/*"',
+  );
+  if (isPR) {
+    // Fetch PR specific references for GitHub
+    execSync(
+      `git fetch origin pull/${nxBranch}/head:refs/remotes/origin/pr/${nxBranch}`,
+    );
+  }
+  // Checkout using the appropriate reference
+  const checkoutRef = isPR ? 'refs/remotes/origin/pr/' + nxBranch : commitSha;
+  execSync(`git checkout --progress --force ${checkoutRef}`);
+} else {
+  // Fetch with specified depth
+  const fetchRef = isPR ? `pull/${nxBranch}/head` : `${nxBranch}`;
+  const tagsArg = fetchTags ? ' --tags' : '--no-tags';
+  execSync(
+    `git fetch ${tagsArg} --prune --progress --no-recurse-submodules --depth=${depth} origin ${fetchRef}`,
+  );
+  // Checkout the branch or PR
+  const checkoutRef = isPR ? 'FETCH_HEAD' : `origin/${nxBranch}`;
+  execSync(`git checkout --progress --force -B ${nxBranch} ${checkoutRef}`);
+}
--- workflow-steps/checkout/output/main.js
+++ workflow-steps/checkout/output/main.js
@@ -2,13 +2,30 @@
 var import_child_process = require("child_process");
 var repoUrl = process.env.GIT_REPOSITORY_URL;
 var commitSha = process.env.NX_COMMIT_SHA;
-var commitRef = process.env.NX_COMMIT_REF;
-var branch = process.env.NX_BRANCH;
+var nxBranch = process.env.NX_BRANCH;
 var depth = process.env.GIT_CHECKOUT_DEPTH || 1;
+var fetchTags = process.env.GIT_FETCH_TAGS === "true";
+var isPR = !isNaN(parseInt(nxBranch));
 (0, import_child_process.execSync)(`git config --global --add safe.directory /home/workflows/workspace`);
 (0, import_child_process.execSync)("git init .");
 (0, import_child_process.execSync)(`git remote add origin ${repoUrl}`);
-(0, import_child_process.execSync)(
-  `git fetch --no-tags --prune --progress --no-recurse-submodules --depth=${depth} origin +${commitSha}:${commitRef}`
-);
-(0, import_child_process.execSync)(`git checkout --progress --force -B ${branch} ${commitRef}`);
+if (depth === "0") {
+  (0, import_child_process.execSync)(
+    'git fetch --prune --progress --no-recurse-submodules --tags origin "+refs/heads/*:refs/remotes/origin/*"'
+  );
+  if (isPR) {
+    (0, import_child_process.execSync)(
+      `git fetch origin pull/${nxBranch}/head:refs/remotes/origin/pr/${nxBranch}`
+    );
+  }
+  const checkoutRef = isPR ? "refs/remotes/origin/pr/" + nxBranch : commitSha;
+  (0, import_child_process.execSync)(`git checkout --progress --force ${checkoutRef}`);
+} else {
+  const fetchRef = isPR ? `pull/${nxBranch}/head` : `${nxBranch}`;
+  const tagsArg = fetchTags ? " --tags" : "--no-tags";
+  (0, import_child_process.execSync)(
+    `git fetch ${tagsArg} --prune --progress --no-recurse-submodules --depth=${depth} origin ${fetchRef}`
+  );
+  const checkoutRef = isPR ? "FETCH_HEAD" : `origin/${nxBranch}`;
+  (0, import_child_process.execSync)(`git checkout --progress --force -B ${nxBranch} ${checkoutRef}`);
+}
