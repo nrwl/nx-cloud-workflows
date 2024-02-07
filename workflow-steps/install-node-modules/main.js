9e4b6ce9b9cbe2c953f39ce5989e79783d9c434b
Victor Savkin <mail@vsavkin.com>
2024-02-07 12:07:41 -0500

    fix jest ts-node issue

--- workflow-steps/install-node-modules/main.js
+++ workflow-steps/install-node-modules/main.js
@@ -4,17 +4,31 @@ const { existsSync } = require('fs');
 if (existsSync('package-lock.json')) {
   console.log('Using npm');
   execSync('npm ci --legacy-peer-deps', { stdio: 'inherit' });
-  console.log('Calculating Nx graph');
-  execSync('npx nx graph --file graph.json');
+  patchJest();
 } else if (existsSync('yarn.lock')) {
   console.log('Using yarn');
   execSync('yarn install --frozen-lockfile', { stdio: 'inherit' });
-  console.log('Calculating Nx graph');
-  execSync('npx nx graph --file graph.json');
+  patchJest();
 } else if (existsSync('pnpm-lock.yaml') || existsSync('pnpm-lock.yml')) {
   // base image has to install pnpm
   console.log('Using pnpm');
   execSync('pnpm install --frozen-lockfile', { stdio: 'inherit' });
-  console.log('Calculating Nx graph');
-  execSync('npx nx graph --file graph.json');
+  patchJest();
+}
+
+function patchJest() {
+  try {
+    const path =
+      'node_modules/jest-config/build/readConfigFileAndSetRootDir.js';
+    const contents = readFileSync(path, 'utf-8');
+    writeFileSync(
+      path,
+      contents.replace(
+        "const tsNode = await import('ts-node');",
+        "require('ts-node'); const tsNode = await import('ts-node');",
+      ),
+    );
+  } catch (e) {
+    console.log('no need to patch jest');
+  }
 }
