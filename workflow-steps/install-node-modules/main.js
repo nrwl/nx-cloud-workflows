e4f888868797a6d76a88cdc8e5fe2391cf2a5884
Victor Savkin <mail@vsavkin.com>
2024-02-07 10:43:25 -0500

    calculate nx graph proactively

--- workflow-steps/install-node-modules/main.js
+++ workflow-steps/install-node-modules/main.js
@@ -4,11 +4,17 @@ const { existsSync } = require('fs');
 if (existsSync('package-lock.json')) {
   console.log('Using npm');
   execSync('npm ci --legacy-peer-deps', { stdio: 'inherit' });
+  console.log('Calculating Nx graph');
+  execSync('npx nx graph --file graph.json');
 } else if (existsSync('yarn.lock')) {
   console.log('Using yarn');
   execSync('yarn install --frozen-lockfile', { stdio: 'inherit' });
+  console.log('Calculating Nx graph');
+  execSync('npx nx graph --file graph.json');
 } else if (existsSync('pnpm-lock.yaml') || existsSync('pnpm-lock.yml')) {
   // base image has to install pnpm
   console.log('Using pnpm');
   execSync('pnpm install --frozen-lockfile', { stdio: 'inherit' });
+  console.log('Calculating Nx graph');
+  execSync('npx nx graph --file graph.json');
 }
