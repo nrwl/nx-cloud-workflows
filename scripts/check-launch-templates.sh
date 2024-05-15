835615d5cd7c2e5e79096ac621e0d9d8b0c01e42
Jonathan Cammisuli <jon@cammisuli.ca>
2024-05-15 12:06:17 -0400

    add validatation checks for step files (#53)

--- scripts/check-launch-templates.sh
+++ scripts/check-launch-templates.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-BASEDIR="./launch-templates"
 
-for FILE in "$BASEDIR"/*
+for FILE in ./launch-templates/*
 do
   if [ -f "$FILE" ]
   then
     echo "Validating $FILE"
     npx nx-cloud validate --workflow-file=$FILE
+  fi
+done
+
+find ./workflow-steps -type f -name "*.yaml" | while read FILE
+do
+  if [ -f "$FILE" ]
+  then
+    echo "Validating $FILE"
+    npx nx-cloud validate --workflow-file=$FILE --step-file
   fi
 done
