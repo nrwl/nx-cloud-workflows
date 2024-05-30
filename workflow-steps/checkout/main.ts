24779e7d7889951e348dfa60f0629a558ae05751
pmariglia <patrick@nrwl.io>
2024-05-30 10:55:53 -0400

    use pwd for safedir instead of hard-coded folder (#56)

--- workflow-steps/checkout/main.ts
+++ workflow-steps/checkout/main.ts
@@ -7,9 +7,7 @@ const depth = process.env.GIT_CHECKOUT_DEPTH || 1;
 const fetchTags = process.env.GIT_FETCH_TAGS === 'true';
 
 if (process.platform != 'win32') {
-  execSync(
-    `git config --global --add safe.directory /home/workflows/workspace`,
-  );
+  execSync(`git config --global --add safe.directory $PWD`);
 }
 execSync('git init .');
 execSync(`git remote add origin ${repoUrl}`);
