d66801407b85b9a639b3ebc247fd1291256d9e95
Rares Matei <matei.rar@gmail.com>
2024-07-30 18:52:34 +0100

    add tests for caching step

--- workflow-steps/cache/hasing-utils.spec.ts
+++ workflow-steps/cache/hasing-utils.spec.ts
@@ -1,5 +1,31 @@
+import { hashKey } from './hashing-utils';
+import * as path from 'path';
+
+console.log(hashKey);
 describe('hashing-utils', () => {
-  it('should work', () => {
-    expect('shop').toEqual('shop');
+  const testDir = path.join(__dirname, 'test-files');
+  it('should hash a single file', () => {
+    expect(hashKey(`${testDir}/yarn.lock`)).toEqual(
+      '6ef0d64a2ac614adc8dac86db67244e77cdad3253a65fb8e2b7c11ed4cbb466a',
+    );
+  });
+
+  it('should hash multiple files', () => {
+    expect(hashKey(`${testDir}/yarn.lock | ${testDir}/main.js`)).toEqual(
+      'e5c39d066ffa2cd0d38165a018aaac3118b1c81d8f4631335e0f19cda6fa8e65',
+    );
+  });
+
+  it('should hash simple strings', () => {
+    expect(
+      hashKey(`${testDir}/yarn.lock | ${testDir}/main.js | "test1"`),
+    ).toEqual(
+      '16b5f107c209ad4d76dffc803beba37b7e836ca2ca229731c8bc88040874a003',
+    );
+    expect(
+      hashKey(`${testDir}/yarn.lock | ${testDir}/main.js | "test2"`),
+    ).toEqual(
+      '226f813c92638665c8daa0920cfb83e5f33732f8843042deee348032a1abee40',
+    );
   });
 });
--- workflow-steps/cache/jest.config.ts
+++ workflow-steps/cache/jest.config.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
 export default {
   displayName: 'cache-step',
+  preset: 'ts-jest',
 };
