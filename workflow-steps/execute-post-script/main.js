f8803d415067cf619490c5ca6b0d0d5898fa550d
Rares Matei <matei.rar@gmail.com>
2024-08-29 16:51:28 +0100

    execute post step

--- workflow-steps/execute-post-script/main.js
+++
@@ -1,6 +0,0 @@
-const { execSync } = require('child_process');
-const { existsSync, readFileSync, writeFileSync } = require('fs');
-
-const command = process.env.NX_CLOUD_INPUT_script;
-
-execSync(command, { stdio: 'inherit' });
