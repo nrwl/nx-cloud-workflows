af5b9a8ea4601a4c3183f99460c3ee0cbe215827
Caleb Ukle <caleb@nrwl.io>
2024-10-28 11:05:15 -0500

    feat(install-node-modules): allow controlling legacy-peer-deps for npm (#72)
    Most people should not being using legacy peer deps anymore, but we
    understand the compat needs and could be a breaking change to just
    change this from under people.
    
    This PR introduces a new input to the install-node-modules step to
    enable/disable the usage of `--legacy-peer-deps` arg for `npm ci`
    install command.
    
    Since it's a lot of overhead to ask people to create a launch template
    just to control 1 input, this also introduces the
    `NX_CLOUD_NPM_LEGACY_INSTALL` env var that can be sent via the main
    agent to controll this behavior as well.

--- workflow-steps/install-node-modules/main.js
+++ workflow-steps/install-node-modules/main.js
@@ -2,6 +2,7 @@ const { execSync } = require('child_process');
 const { existsSync, readFileSync, writeFileSync } = require('fs');
 
 const command = getInstallCommand();
+
 if (command) {
   console.log(`Installing dependencies using ${command.split(' ')[0]}`);
   console.log(`  Running command: ${command}\n`);
@@ -15,7 +16,22 @@ if (command) {
 
 function getInstallCommand() {
   if (existsSync('package-lock.json')) {
-    return 'npm ci --legacy-peer-deps';
+    const legacyInstallInput =
+      process.env.NX_CLOUD_INPUT_npm_legacy_install ||
+      // allow controlling via env var form main agent so people don't need to make a custom launch template just for 1 input
+      process.env.NX_CLOUD_NPM_LEGACY_INSTALL;
+
+    if (
+      legacyInstallInput !== undefined &&
+      (legacyInstallInput === 'false' || legacyInstallInput === false)
+    ) {
+      return 'npm ci';
+    } else {
+      console.log(
+        "Installing with --legacy-peer-deps for compatiblity, set the npm_legacy_install step input or NX_CLOUD_NPM_LEGACY_INSTALL env var to 'false' to disable.",
+      );
+      return 'npm ci --legacy-peer-deps';
+    }
   } else if (existsSync('yarn.lock')) {
     const [major] = execSync(`yarn --version`, {
       encoding: 'utf-8',
