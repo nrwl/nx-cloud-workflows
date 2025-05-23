87b1d67fca073383a0a7dc2d471ed704e637f134
Victor Savkin <mail@vsavkin.com>
2025-05-23 10:52:25 -0400

    feat(steps): handle branch checkout (#96)

--- workflow-steps/checkout/output/main.js
+++ workflow-steps/checkout/output/main.js
@@ -11,14 +11,20 @@ if (process.platform != "win32") {
 (0, import_child_process.execSync)("git init .");
 (0, import_child_process.execSync)(`git remote add origin ${repoUrl}`);
 (0, import_child_process.execSync)(`echo "GIT_REPOSITORY_URL=''" >> $NX_CLOUD_ENV`);
-if (depth === "0") {
+if (commitSha.startsWith("origin/")) {
   (0, import_child_process.execSync)(
-    'git fetch --prune --progress --no-recurse-submodules --tags origin "+refs/heads/*:refs/remotes/origin/*"'
+    `git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin ${nxBranch}`
   );
 } else {
-  const tagsArg = fetchTags ? " --tags" : "--no-tags";
-  (0, import_child_process.execSync)(
-    `git fetch ${tagsArg} --prune --progress --no-recurse-submodules --depth=${depth} origin ${commitSha}`
-  );
+  if (depth === "0") {
+    (0, import_child_process.execSync)(
+      'git fetch --prune --progress --no-recurse-submodules --tags origin "+refs/heads/*:refs/remotes/origin/*"'
+    );
+  } else {
+    const tagsArg = fetchTags ? " --tags" : "--no-tags";
+    (0, import_child_process.execSync)(
+      `git fetch ${tagsArg} --prune --progress --no-recurse-submodules --depth=${depth} origin ${commitSha}`
+    );
+  }
 }
 (0, import_child_process.execSync)(`git checkout --progress --force -B ${nxBranch} ${commitSha}`);
